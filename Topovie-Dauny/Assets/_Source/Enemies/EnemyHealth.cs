0df413096baf9e30a6961623c59998089600a101
MeylishDLC <meylishy20@gmail.com>
2024-09-15 23:42:26 +0300

    feat: basic enemy HP implementation

---
+++ Topovie-Dauny/Assets/_Source/Enemies/EnemyHealth.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Pathfinding;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class EnemyHealth: MonoBehaviour
+    {
+        [SerializeField] private int maxHealth;
+        [SerializeField] private int maxMoneyDrop;
+
+        [SerializeField] private AIPath aiPathComponent;
+        
+        private int _currentHealth;
+        private bool _isDead;
+        private void Start()
+        {
+            _currentHealth = maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (!_isDead)
+            {
+                _currentHealth -= damage;
+                Debug.Log(_currentHealth);
+                CheckIfDeadAsync().Forget();
+            }
+        }
+
+        private async UniTask CheckIfDeadAsync()
+        {
+            if (_currentHealth <= 0)
+            {
+                _isDead = true;
+                aiPathComponent.enabled = false;
+                await gameObject.transform.DOScaleX(0f, 0.5f).ToUniTask();
+                
+                DropMoney();
+                Destroy(gameObject);
+            }
+        }
+
+        private void DropMoney()
+        {
+            //todo: implement dropping money
+        }
+    }
+}
--- Topovie-Dauny/Assets/_Source/Weapons/Test-Gun/Bullet.cs
+++ Topovie-Dauny/Assets/_Source/Weapons/Test-Gun/Bullet.cs
@@ -1,9 +1,12 @@
+using System;
+using Enemies;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
     [Range(1, 10)] [SerializeField] private float speed;
+    [SerializeField] private int damageAmount;
     [SerializeField] private float lifetime;
 
     private Rigidbody2D _rb;
@@ -18,12 +21,17 @@ public class Bullet : MonoBehaviour
     {
         _rb.velocity = transform.right * speed;
     }
-
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.layer != LayerMask.NameToLayer("Player"))
         {
             Destroy(gameObject);
+        }
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            var enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+            enemyHealth.TakeDamage(damageAmount);
         }
     }
 }
