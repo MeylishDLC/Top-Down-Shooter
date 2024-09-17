c29f2cbdb8ab52ac41ac1cc33af8bd301ade7654
MeylishDLC <meylishy20@gmail.com>
2024-09-17 17:23:05 +0300

    feat: basic range detector

---
+++ Topovie-Dauny/Assets/_Source/Core/RangeDetector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core
+{
+    public class RangeDetector: MonoBehaviour
+    {
+        public event Action OnPlayerEnterRange;
+        public event Action OnPlayerExitRange;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                OnPlayerEnterRange?.Invoke();
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                OnPlayerExitRange?.Invoke();
+            }
+        }
+    }
+}
