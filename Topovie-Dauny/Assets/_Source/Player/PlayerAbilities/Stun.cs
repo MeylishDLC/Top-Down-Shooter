cbf3ad66fbbcc5d97811986011333098312b6570
MeylishDLC <meylishy20@gmail.com>
2024-09-20 02:47:44 +0300

    feat: stun ability

---
+++ Topovie-Dauny/Assets/_Source/Player/PlayerAbilities/Stun.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Weapons.AbilityWeapons;
+using Zenject;
+
+namespace Player.PlayerAbilities
+{
+    public class Stun: Ability
+    {
+        [SerializeField] private StunZone stunZonePrefab;
+
+        private Transform _playerTransform;
+
+        [Inject]
+        public void Construct(PlayerMovement.PlayerMovement _playerMovement)
+        {
+            _playerTransform = _playerMovement.transform;
+        }
+        public override void UseAbility()
+        {
+            UseAbilityAsync(CancellationToken.None).Forget();
+        }
+
+        private async UniTask UseAbilityAsync(CancellationToken token)
+        {
+            CanUse = false;
+            Instantiate(stunZonePrefab, _playerTransform.position, Quaternion.identity);
+            await UniTask.Delay(CooldownMilliseconds, cancellationToken: token);
+            CanUse = true;
+        }
+    }
+}
---
+++ Topovie-Dauny/Assets/_Source/Weapons/AbilityWeapons/StunZone.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Enemies;
+using Ink.Parsed;
+using Pathfinding;
+using UnityEngine;
+
+namespace Weapons.AbilityWeapons
+{
+    public class StunZone: MonoBehaviour
+    {
+        [SerializeField] private int damage;
+        [SerializeField] private float stunDuration;
+
+        private List<AIPath> _enemiesAffected = new();
+        private Collider2D _collider;
+        private void Start()
+        {
+            _collider = GetComponent<Collider2D>();
+            UnStunEnemiesAfterDuration(CancellationToken.None).Forget();
+        }
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            {
+                if (other.TryGetComponent(out AIPath aiPath))
+                {
+                    aiPath.canMove = false;
+                    var enemyHealth = other.GetComponent<EnemyHealth>();
+                    enemyHealth.TakeDamage(damage);
+
+                    if (!_enemiesAffected.Contains(aiPath))
+                    {
+                        _enemiesAffected.Add(aiPath);
+                    }
+                }
+            }
+        }
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            {
+                if (other.TryGetComponent(out AIPath aiPath))
+                {
+                    aiPath.canMove = false;
+                    
+                    if (!_enemiesAffected.Contains(aiPath))
+                    {
+                        _enemiesAffected.Add(aiPath);
+                    }
+                }
+            }
+        }
+        private async UniTask UnStunEnemiesAfterDuration(CancellationToken token)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(stunDuration), cancellationToken: token);
+
+            _collider.enabled = false;
+            foreach (var enemy in _enemiesAffected)
+            {
+                if (enemy != null)
+                {
+                    enemy.canMove = true;
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
