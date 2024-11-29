e63f3b6503384725518b200c1a9ecaef0b881396
MeylishDLC <meylishy20@gmail.com>
2024-11-29 13:21:38 +0300

    tweak: added fireballs boss attack

---
+++ Topovie-Dauny/Assets/_Source/Enemies/Boss/BossAttacks/Fireballs/FireballsAttack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using Bullets.BulletPatterns;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Enemies.Boss.BossAttacks.Fireballs
+{
+    public class FireballsAttack: BulletSpawner, IBossAttack
+    {
+        [SerializeField] private float minAttackDuration;
+        [SerializeField] private float maxAttackDuration;
+        
+        CancellationToken _destroyCancellationToken;
+        private void Awake()
+        {
+            _destroyCancellationToken = this.GetCancellationTokenOnDestroy();
+            enabled = false;
+        }
+
+        public UniTask TriggerAttack(CancellationToken token)
+        {
+            enabled = true;
+            var duration = GetRandomDuration();
+            return UniTask.Delay(TimeSpan.FromSeconds(duration), cancellationToken: _destroyCancellationToken)
+                .ContinueWith(() => enabled = false);
+        }
+
+        private float GetRandomDuration()
+        {
+            return Random.Range(minAttackDuration, maxAttackDuration);
+        }
+    }
+}
