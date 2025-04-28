5b61a14e3e8c0aa576c6e863fee04993bd731466
MeylishDLC <meylishy20@gmail.com>
2025-04-28 15:42:01 +0300

    fix: test scene fixes

---
+++ Topovie-Dauny/Assets/_Source/UI/Menus/TestSceneButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Core.SceneManagement;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace UI.Menus
+{
+    public class TestSceneButton: MonoBehaviour
+    {
+        [SerializeField] private int testSceneIndex;
+        private Button _button;
+        private SceneLoader _sceneLoader;
+
+        [Inject]
+        public void Construct(SceneLoader sceneLoader)
+        {
+            _sceneLoader = sceneLoader;
+        }
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+            _button.onClick.AddListener(OpenTestScene);
+        }
+        private void OpenTestScene()
+        {
+            _button.interactable = false;
+            _sceneLoader.LoadSceneAsync(testSceneIndex, false).Forget();
+        }
+    }
+}
