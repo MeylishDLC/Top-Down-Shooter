1039c1da517764db41fb5227aad5a2ba9c761295
MeylishDLC <meylishy20@gmail.com>
2025-05-29 13:23:56 +0300

    feat: pencil animation during charging

---
+++ Topovie-Dauny/Assets/_Source/GameEnvironment/ChargerAnimator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameEnvironment
+{
+    public class ChargerAnimator: MonoBehaviour
+    {
+        [SerializeField] private RangeDetector rangeDetector;
+        
+        private static readonly int ChargeStartProperty = Animator.StringToHash("chargeStart");
+        private Animator _animator;
+        private PortalChargerTrigger _portalChargerTrigger;
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _portalChargerTrigger = rangeDetector.gameObject.GetComponent<PortalChargerTrigger>();
+            
+            rangeDetector.OnPlayerEnterRange += ResumeAnimation;
+            rangeDetector.OnPlayerExitRange += StopAnimation;
+            _portalChargerTrigger.OnChargePortalPressed += StartAnimation;
+        }
+        private void OnDestroy()
+        {
+            rangeDetector.OnPlayerEnterRange -= ResumeAnimation;
+            rangeDetector.OnPlayerExitRange -= StopAnimation;
+            _portalChargerTrigger.OnChargePortalPressed -= StartAnimation;
+        }
+        private void StartAnimation(int _)
+        {
+            _portalChargerTrigger.OnChargePortalPressed -= StartAnimation;
+            _animator.SetTrigger(ChargeStartProperty);
+        }
+        private void StopAnimation()
+        {
+            if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f && _animator.speed != 0f)
+            {
+                _animator.speed = 0f;
+            }
+        }
+        private void ResumeAnimation()
+        {
+            _animator.speed = 1f;
+        }
+    }
+}
